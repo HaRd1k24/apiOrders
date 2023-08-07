d8c6484ac1a2a90ab4cdcc68b4c1ce194a246889
Трапезников Лев Александрович <LAlTrapeznikov@sberbank.ru>
2023-08-07 12:48:23 +0300

    reload

--- src/main/java/ru/lev/controller/OrdersController.java
+++ src/main/java/ru/lev/controller/OrdersController.java
@@ -1,40 +1,38 @@
 package ru.lev.controller;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import ru.lev.dao.ElectronicSigarsDao;
+import ru.lev.dao.ElectronicSigarsDaoImpl;
 import ru.lev.model.Buyer;
 
 
 @RestController
+@RequiredArgsConstructor
 public class OrdersController {
 
-    private ElectronicSigarsDao electronicSigarsDao;
+    private final ElectronicSigarsDaoImpl electronicSigarsDao;
 
-    @Autowired
-    public OrdersController(ElectronicSigarsDao electronicSigarsDao) {
-        this.electronicSigarsDao = electronicSigarsDao;
-    }
 
-    @GetMapping(value = "/orders", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/v1/orders", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public String availableOrders(@RequestBody Buyer buyer) {
         return "Для заказа доступно: " + electronicSigarsDao.countTaste(buyer.getNameSigares());
     }
 
-    @PostMapping(value = "/create/orders", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/v1/orders/buy", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public String buyHQD(@RequestBody Buyer buyer) {
         return electronicSigarsDao.buyHqd(buyer.getNameSigares(), buyer.getCount());
     }
 
-    @PostMapping(value = "/orders/zavoz", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/v1/orders/create", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public String putElectronicESsigars(@RequestBody Buyer buyer) {
         electronicSigarsDao.create(buyer.getNameSigares(), buyer.getCount());
         return "Вы завезли в базу:  " + buyer.getNameSigares() + " в количестве: " + buyer.getCount();
     }
 
-    @PutMapping(value = "/orders/putZavoz",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "/v1/orders/refreshCount",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
     public String put(@RequestBody Buyer buyer){
         electronicSigarsDao.updateElectronigSigars(buyer.getNameSigares(),buyer.getCount());
         return "Вы завезли на базу: " + buyer.getNameSigares() + "кол-во:" + buyer.getCount();
--- src/main/java/ru/lev/util/HibernateUtil.java
+++ src/main/java/ru/lev/util/HibernateUtil.java
@@ -11,10 +11,15 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 
     public static SessionFactory buildSessionFactory() {
-        Configuration configuration = new Configuration();
+        Configuration configuration = buildConfiguration();
         configuration.configure();
-        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
 
         return configuration.buildSessionFactory();
+    }
+
+    public static Configuration buildConfiguration(){
+        Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        return configuration;
     }
 }
